f8e0ad2a1d55ff9e9d876cbbf1e223842ce92986
Robert Armstrong <robert@code-crew.org>
2022-06-10 11:33:26 -0500

    added both mult and add funcs

--- app.js
+++ app.js
@@ -1,53 +1,75 @@
 'use strict';
 /////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
- Write a function called sum() that takes in two numbers as arguments and then returns an array 
- where the first element is the sum of those numbers,
- and the second element is a concatenated string that EXACTLY follows this example 
- and uses the values that were input into the function:
+   /* 
+Write a function called sum() 
+that takes in two numbers as arguments
+and then returns an array ---
+where the first element is the sum of those numbers,
+and the second element is a concatenated string that EXACTLY follows this example 
+and uses the values that were input into the function:
 "The sum of 4 and 7 is 11."
-Test this function by hand in the console to get it working,
- and when you think it is finished, uncomment the call for the testSum() function 
- below and check the console to see if the test passes.*/
+*/
 
 // Write your code here
-function testSum(a, b) { 
-let sum = a+b
-let msg = `the sum of ${a} and ${b} is sum`
-let retArray = [sum,msg]
-return retArray
+function sum(a, b) { //eslint-disable-line
+let mySum = a+b;
+let mySumString = `The sum of ${a} and ${b} is ${mySum}`
+let myReturnArray = [mySum,mySumString]
+
+
+return myReturnArray
 }
 
 // Here is the test for sum(); uncomment it to run it
-console.log( testSum(4, 7))
+console.log(sum(4,7))
 
-// Once you get the test passing, do an a-c-p cycle
-// and synchronize the code between GitHub and your laptop.
-// Don't forget to create a new branch for your work on the next question!
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() 
+that takes in two numbers as arguments 
+and returns an array
+where the first element is the product of those numbers, 
+and the second element is a string that EXACTLY follows this example
+and uses the values that were input into the function:
 "The product of 5 and 9 is 45."
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+let product = a*b;
+
+let productString = `The product of ${a} and ${b} is ${product}.`;
+
+let myMultipliedArray = [product, productString];
+
+return myMultipliedArray
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+console.log(multiply(5,9));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() 
+that takes in three numbers as separate arguments 
+and returns an array 
+where the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  
+and the third and fourth elements are strings
+that EXACTLY follow this example and use the values that were input into the function:
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function.
+To do addition, use your sum() function,
+and to do multiplication, use your multiply() function that you've already created.
+You're going to have to be resourceful to figure out how to do this.
+However, you may continue to use the + operator for string concatenation.
+*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
